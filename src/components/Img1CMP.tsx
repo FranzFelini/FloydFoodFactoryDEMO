a773ada7cc8c3d65599b2b3a1a34979931418b7f
franzfelini <arnautovic.feda@gmail.com>
2024-09-23 23:08:41 +0200

    fix: image

--- src/components/Img1CMP.tsx
+++ src/components/Img1CMP.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-
 export default function Prikaz() {
   return (
-    <section className="p-8 z-20">
-      <Image
-        src="/floyd-logo.png"
-        alt=""
-        className="max-w-full brightness-200"
-      />
+    <section className="p-8 z-20 ">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/floyd-logo.png" alt="" className="max-w-full brightness-200" />
     </section>
   );
 }
